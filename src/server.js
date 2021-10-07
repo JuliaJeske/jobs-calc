ee8a241d59cccddcfcfa6912d4bf666899fe40b3
Julia Jeske <julia.jeske01@gmail.com>
2021-10-06 21:26:40 -0300

    feat: refactor and new features

--- src/server.js
+++ src/server.js
@@ -7,6 +7,9 @@ server.set('view engine', "ejs")
 //habilitar arquivos estaticos
 server.use(express.static('public'))
 
+//habilitar req.body
+server.use(express.urlencoded({extended:true}))
+
 //routes
 server.use(routes)
 
